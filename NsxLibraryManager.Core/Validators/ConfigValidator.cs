d76eeef7f6d5e620b50b944bff8f5310fe108b71
Ivan Villareal <ivaano@gmail.com>
2024-01-05 14:54:07 -0800

    configvalidator

--- NsxLibraryManager.Core/Validators/ConfigValidator.cs
+++ NsxLibraryManager.Core/Validators/ConfigValidator.cs
@@ -1,3 +1,5 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
 using NsxLibraryManager.Core.Settings;
@@ -8,17 +10,59 @@ public static class ConfigValidator
 {
     public static bool Validate(ConfigurationManager configurationManager)
     {
-        if (configurationManager["NsxLibraryManager:LibraryPath"] != string.Empty || configurationManager["NsxLibraryManager:LibraryPath"] is not null)
+        if (File.Exists(configurationManager["NsxLibraryManager:TitleDatabase"]))
+            return true;
+        
+        CreateDefaultConfigFile(AppConstants.ConfigFileName);
+/*
+        if (configurationManager["NsxLibraryManager:LibraryPath"] != string.Empty 
+            || configurationManager["NsxLibraryManager:LibraryPath"] is not null)
         {
+            if (File.Exists(configurationManager["NsxLibraryManager:TitleDatabase"]))
             return true;
         }
-        
-        return false;
+*/   
+        return true;
     }
     
     public static bool Validate(IOptions<AppSettings> configAppSettings)
     {
         //builder.Configuration["NsxLibraryManager:LibraryPath"] != string.Empty || builder.Configuration["NsxLibraryManager:LibraryPath"] is not null
         return true;
+    }
+    
+    public static bool CreateDefaultConfigFile(string configFileName)
+    {
+        var jsonWriteOptions = new JsonSerializerOptions
+        {
+            WriteIndented = true
+        };
+        jsonWriteOptions.Converters.Add(new JsonStringEnumConverter());
+
+        var appSettings = new AppSettings()
+        {
+            TitleDatabase = Path.Combine(AppContext.BaseDirectory, "title.db"),
+            LibraryPath = Path.Combine(AppContext.BaseDirectory, "library"),
+            DownloadSettings = new DownloadSettings()
+            {
+                TitleDbPath = Path.Combine(AppContext.BaseDirectory, "titledb"),
+                TimeoutInSeconds = 100,
+                RegionUrl = "https://raw.githubusercontent.com/blawar/titledb/master/{region}.en.json",
+                CnmtsUrl = "https://raw.githubusercontent.com/blawar/titledb/master/cnmts.json",
+                VersionsUrl = "https://raw.githubusercontent.com/blawar/titledb/master/versions.json",
+                Regions = new List<string> {"US"}
+            }
+        };
+        
+        var sectionName = new Dictionary<string, AppSettings>
+        {
+            { AppConstants.AppSettingsSectionName, appSettings }
+        };
+        
+        var newJson = JsonSerializer.Serialize(sectionName, jsonWriteOptions);
+        var appSettingsPath = Path.Combine(AppContext.BaseDirectory, AppConstants.ConfigFileName);
+        File.WriteAllText(appSettingsPath, newJson);
+        
+        return false;
     }
 }
--- NsxLibraryManager/Program.cs
+++ NsxLibraryManager/Program.cs
@@ -16,82 +16,98 @@ using Radzen;
 Console.OutputEncoding = Encoding.UTF8;
 
 
-var builder = WebApplication.CreateBuilder(args);
-
-// configuration.
-builder.Services
+var builderConfig = WebApplication.CreateBuilder(args);
+builderConfig.Services
     .AddOptions<AppSettings>()
     .BindConfiguration(AppConstants.AppSettingsSectionName);
-    //.ValidateDataAnnotations()
-    //.ValidateOnStart();
-builder.Configuration
+//.ValidateDataAnnotations()
+//.ValidateOnStart();
+builderConfig.Configuration
     .AddJsonFile(AppConstants.ConfigFileName,
         optional: true,
         reloadOnChange: true);
 
-var validConfig = ConfigValidator.Validate(builder.Configuration);
-builder.Configuration.AddInMemoryCollection(
-    new Dictionary<string, string?>
+var valid = ConfigValidator.Validate(builderConfig.Configuration);
+
+if (valid)
+{
+    var builder = WebApplication.CreateBuilder(args);
+
+    // configuration.
+    builder.Services
+        .AddOptions<AppSettings>()
+        .BindConfiguration(AppConstants.AppSettingsSectionName);
+        //.ValidateDataAnnotations()
+        //.ValidateOnStart();
+    builder.Configuration
+        .AddJsonFile(Path.Combine(AppContext.BaseDirectory, AppConstants.ConfigFileName),
+            optional: true,
+            reloadOnChange: true);
+
+    var validConfig = ConfigValidator.Validate(builder.Configuration);
+    builder.Configuration.AddInMemoryCollection(
+        new Dictionary<string, string?>
+        {
+            { "IsConfigValid", validConfig.ToString() }
+        });
+
+    builder.Services.AddAutoMapper(typeof(MappingProfile));
+    builder.Services.AddHttpClient();
+    builder.Services.AddRazorPages();
+    builder.Services.AddServerSideBlazor();
+    //nsx services
+    if (validConfig)
     {
-        { "IsConfigValid", validConfig.ToString() }
-    });
+        builder.Services.AddSingleton<IDataService, DataService>();
+        builder.Services.AddSingleton<ITitleLibraryService, TitleLibraryService>();
+        builder.Services.AddSingleton<ITitleDbService, TitleDbService>();
+        builder.Services.AddSingleton<IDownloadService, DownloadService>();
+        builder.Services.AddSingleton<IFileInfoService, FileInfoService>();
+        builder.Services.AddSingleton<IKeySetProviderService, KeySetProviderService>();
+        builder.Services.AddSingleton<IPackageTypeAnalyzer, PackageTypeAnalyzer>();
+        builder.Services.AddSingleton<IPackageInfoLoader, PackageInfoLoader>();
+        builder.Services.AddScoped<IRenamerService, RenamerService>();
+        builder.Services.AddScoped<IValidator<RenamerSettings>, RenamerSettingsValidator>();    
+    }
 
-builder.Services.AddAutoMapper(typeof(MappingProfile));
-builder.Services.AddHttpClient();
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
-//nsx services
-if (validConfig)
-{
-    builder.Services.AddSingleton<IDataService, DataService>();
-    builder.Services.AddSingleton<ITitleLibraryService, TitleLibraryService>();
-    builder.Services.AddSingleton<ITitleDbService, TitleDbService>();
-    builder.Services.AddSingleton<IDownloadService, DownloadService>();
-    builder.Services.AddSingleton<IFileInfoService, FileInfoService>();
-    builder.Services.AddSingleton<IKeySetProviderService, KeySetProviderService>();
-    builder.Services.AddSingleton<IPackageTypeAnalyzer, PackageTypeAnalyzer>();
-    builder.Services.AddSingleton<IPackageInfoLoader, PackageInfoLoader>();
-    builder.Services.AddScoped<IRenamerService, RenamerService>();
-    builder.Services.AddScoped<IValidator<RenamerSettings>, RenamerSettingsValidator>();    
+    //radzen services
+    builder.Services.AddRadzenComponents();
+
+    var modelBuilder = new ODataConventionModelBuilder();
+    modelBuilder.EntitySet<Game>("Games");
+
+    builder.Services.AddControllers().AddOData(
+            options => options.Select().Filter().OrderBy().Expand().Count().SetMaxTop(null).AddRouteComponents(
+                    "odata",
+                    modelBuilder.GetEdmModel()));
+
+    //Static web assets for linux development https://learn.microsoft.com/en-us/aspnet/core/razor-pages/ui-class?view=aspnetcore-7.0&tabs=visual-studio#consume-content-from-a-referenced-rcl
+    if (builder.Environment.IsEnvironment("DeveLinux"))
+    {
+        builder.WebHost.UseStaticWebAssets();
+    }
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    //app.UseHttpsRedirection();
+
+    app.UseStaticFiles();
+
+    app.UseRouting();
+
+    app.MapControllers();
+
+    app.MapBlazorHub();
+    app.MapFallbackToPage("/_Host");
+
+
+    app.Run();    
 }
-
-//radzen services
-builder.Services.AddRadzenComponents();
-
-var modelBuilder = new ODataConventionModelBuilder();
-modelBuilder.EntitySet<Game>("Games");
-
-builder.Services.AddControllers().AddOData(
-        options => options.Select().Filter().OrderBy().Expand().Count().SetMaxTop(null).AddRouteComponents(
-                "odata",
-                modelBuilder.GetEdmModel()));
-
-//Static web assets for linux development https://learn.microsoft.com/en-us/aspnet/core/razor-pages/ui-class?view=aspnetcore-7.0&tabs=visual-studio#consume-content-from-a-referenced-rcl
-if (builder.Environment.IsEnvironment("DeveLinux"))
-{
-    builder.WebHost.UseStaticWebAssets();
-}
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-//app.UseHttpsRedirection();
-
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.MapControllers();
-
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
-
-
-app.Run();
