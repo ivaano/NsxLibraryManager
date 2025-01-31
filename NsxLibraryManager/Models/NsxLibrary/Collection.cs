86c76b07414aebc1546422760919fd8910ab0933
Ivan Villareal <ivaano@gmail.com>
2025-01-31 08:17:54 -0800

    Adding Collection Table

---
+++ NsxLibraryManager/Models/NsxLibrary/Collection.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NsxLibraryManager.Models.NsxLibrary;
+
+public class Collection
+{
+    [Required]
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; init; }
+    
+    [Column(TypeName = "VARCHAR")]
+    public required string Name { get; init; }
+    
+    public virtual ICollection<Title> Titles { get; set; } = [];
+}
