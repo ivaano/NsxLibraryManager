c27b3d65303fb0c74927baec199c3ddbf3ce3cff
Ivan Villareal <ivaano@gmail.com>
2025-02-07 16:36:43 -0800

    Mass Edit Titles (for collections) [wip]

---
+++ NsxLibraryManager/Pages/Components/TitleMassEditDialog.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using NsxLibraryManager.Shared.Dto;
+
+namespace NsxLibraryManager.Pages.Components;
+
+public partial class TitleMassEditDialog : ComponentBase
+{
+    [Parameter]
+    public IList<LibraryTitleDto> SelectedTitles { get; set; } = null!;
+    
+    
+    protected override async Task OnParametersSetAsync()
+    {
+        var hola = false;
+    }
+}
