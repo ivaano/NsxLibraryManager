b17c6646c68adbd2a6f6d66323eb7f05dad620d9
Ivan Villareal <ivaano@gmail.com>
2025-02-08 23:08:34 -0800

    Duplicate Titles Lists by ContentType

--- NsxLibraryManager/Pages/DuplicateTitles.razor.cs
+++ NsxLibraryManager/Pages/DuplicateTitles.razor.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Components;
 using NsxLibraryManager.Services.Interface;
 using NsxLibraryManager.Shared.Dto;
+using NsxLibraryManager.Shared.Enums;
+using Radzen;
 using Radzen.Blazor;
 
 namespace NsxLibraryManager.Pages;
@@ -10,26 +12,58 @@ public partial class DuplicateTitles : ComponentBase
     [Inject] private ITitleLibraryService TitleLibraryService { get; set; } = default!; 
 
     private int selectedTabIndex = 0;
-    private RadzenDataGrid<LibraryTitleDto> _duplicateTitlesGrid = default!;
-    private IEnumerable<LibraryTitleDto> _duplicateTitles = default!;
+    private RadzenDataGrid<LibraryTitleDto> _duplicateBaseTitlesGrid = default!;
+    private RadzenDataGrid<LibraryTitleDto> _duplicateUpdatesTitlesGrid = default!;
+    private RadzenDataGrid<LibraryTitleDto> _duplicateDlcTitlesGrid = default!;
+    private IEnumerable<LibraryTitleDto> _duplicateBaseTitles = default!;
+    private IEnumerable<LibraryTitleDto> _duplicateUpdatesTitles = default!;
+    private IEnumerable<LibraryTitleDto> _duplicateDlcTitles = default!;
     private bool isLoading = false;
     private readonly IEnumerable<int> _pageSizeOptions = new[] { 25, 50, 100 };
-    private int _count;
+    private int _countBaseTitles;
+    private int _countUpdatesTitles;
+    private int _countDlcTitles;
     private async Task LoadFiles()
     {
         try
         {
             isLoading = true;
-            var duplicateResult = await TitleLibraryService.GetFirstDuplicateTitles();
-            if (duplicateResult.IsSuccess)
+            if (selectedTabIndex == 0)
             {
-                _count = duplicateResult.Value.Count;
-                _duplicateTitles = duplicateResult.Value.Titles;
+                var duplicateResult = await TitleLibraryService.GetFirstDuplicateTitles(TitleContentType.Base);
+                if (duplicateResult.IsSuccess)
+                {
+                    _countBaseTitles = duplicateResult.Value.Count;
+                    _duplicateBaseTitles = duplicateResult.Value.Titles;
+                }
+                else
+                {
+                }    
             }
-            else
+            if (selectedTabIndex == 1)
             {
+                var duplicateResult = await TitleLibraryService.GetFirstDuplicateTitles(TitleContentType.Update);
+                if (duplicateResult.IsSuccess)
+                {
+                    _countUpdatesTitles = duplicateResult.Value.Count;
+                    _duplicateUpdatesTitles = duplicateResult.Value.Titles;
+                }
+                else
+                {
+                }    
             }
-                
+            if (selectedTabIndex == 2)
+            {
+                var duplicateResult = await TitleLibraryService.GetFirstDuplicateTitles(TitleContentType.DLC);
+                if (duplicateResult.IsSuccess)
+                {
+                    _countDlcTitles = duplicateResult.Value.Count;
+                    _duplicateDlcTitles = duplicateResult.Value.Titles;
+                }
+                else
+                {
+                }    
+            }                
         }
         finally
         {
