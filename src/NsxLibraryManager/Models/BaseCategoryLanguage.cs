caec0e1edc4202eeb67d8f3e9cfc1289302bec18
Ivan Villareal <ivaano@gmail.com>
2025-07-10 12:04:44 -0700

    Warning cleanup (#104)
    * warning cleanup
    
    * Null validations
    
    * Mor null validations
    
    * null warning fixes

--- src/NsxLibraryManager/Models/BaseCategoryLanguage.cs
+++ src/NsxLibraryManager/Models/BaseCategoryLanguage.cs
@@ -14,15 +14,15 @@ public class BaseCategoryLanguage
     
     [Column(TypeName = "VARCHAR")]
     [StringLength(2)]
-    public string Region { get; set; }
+    public required string Region { get; set; }
 
     [Column(TypeName = "VARCHAR")]
     [StringLength(2)]
-    public string Language { get; set; }
+    public required string Language { get; set; }
 
     [Column(TypeName = "VARCHAR")]
     [StringLength(30)]
-    public string Name { get; set; }
+    public required string Name { get; set; }
     
     public int CategoryId { get; set; }
    
